2c377d9a931b2cdf80e4de61ddceb33e49c0ede1
Leestons <leestons@gmail.com>
2017-01-03 04:02:02 +0000

    Tidied up the code somewhat

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -3,36 +3,28 @@ parsed = False
 
 def game():
 	secretNumber = random.randint(1, 20)
-	print('I am thinking of a number between 1 and 20.')
-	
-	#Ask the player to guess 6 times
+	print('I am thinking of a number between 1 and 20')
 	while parsed == False:
 		try:
-			for guessesTaken in range(1, 7):
-				print('Take a guess.')
-				guess = int(input())
-				
+			for guessesTaken in range(1,7):
+				guess = int(input('Take a guess '))
 				if guess < secretNumber:
-					print('Your guess is too low.')
+					print('Your guess is too low')
 				elif guess > secretNumber:
-					print('Your guess is too high.')
+					print('Your guess is too high')
 				else:
-					break # This is the correct answer!
-				
+					break
 			if guess == secretNumber:
-				print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
+				print('You guessed my number in ' + str(guessesTaken) + ' guesses!')
 			else:
-				print('Nope. The number I was thinking of was ' + str(secretNumber))
-			print('Press Y to play again, press any other key to exit')
-			response = input()
-			if response == 'y':
+				print('The number I was thinking of was ' +str(secretNumber))
+			replay = input('Press Y to play again, press any other key to quit: ')
+			if replay == 'y':
 				game()
 			else:
 				print('Goodbye!')
 				sys.exit()
 		except ValueError:
-			print('Please enter a number')
+			print('Please enter a number')	
+
 game()
-
-	
-
