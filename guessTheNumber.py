75c44340a769e9bda57101699fad19ac9b66cf02
Leestons <leestons@gmail.com>
2017-01-04 03:11:03 +0000

    Added difficulty

--- guessTheNumber.py
+++ guessTheNumber.py
@@ -1,38 +1,31 @@
 import random, sys
 parsed = False
+difficulty = int(input('What number would you like to go up to? '))
 
 def game():
-	secretNumber = random.randint(1, 20)
-	print('I am thinking of a number between 1 and 20.')
-	
-	#Ask the player to guess 6 times
+	secretNumber = random.randint(1, difficulty)
+	print('I am thinking of a number between 1 and ' + str(difficulty))
 	while parsed == False:
 		try:
-			for guessesTaken in range(1, 7):
-				print('Take a guess.')
-				guess = int(input())
-				
+			for guessesTaken in range(1,7):
+				guess = int(input('Take a guess '))
 				if guess < secretNumber:
-					print('Your guess is too low.')
+					print('Your guess is too low')
 				elif guess > secretNumber:
-					print('Your guess is too high.')
+					print('Your guess is too high')
 				else:
-					break # This is the correct answer!
-				
+					break
 			if guess == secretNumber:
 				print('Good job! You guessed my number in ' + str(guessesTaken) + ' guesses!')
 			else:
-				print('Nope. The number I was thinking of was ' + str(secretNumber))
-			print('Enter Y to play again, press any other key to exit')
-			response = input()
-			if response == 'y':
+				print('The number I was thinking of was ' +str(secretNumber))
+			replay = input('Press Y to play again, press any other key to quit: ')
+			if replay == 'y':
 				game()
 			else:
 				print('Goodbye!')
 				sys.exit()
 		except ValueError:
-			print('Please enter a number')
+			print('Please enter a number')	
+
 game()
-
-	
-
